3b571f57663739ca5c558015df7d02d5a28ff062
Marcel Klehr <mklehr@gmx.net>
2015-07-31 17:29:12 +0200

    Revamp broadcast API
    Provide a sync channel for distributing edits between workers
    and a document channel, which will be available in the client

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
+var through = require('through2')
+
 module.exports = setup
 module.exports.provides = ['broadcast']
 
 function setup(plugin, imports, register) {
-  register(null, {
-    broadcast: require('./broadcast')
-  })
+  var docStreams = {}
+    , syncStreams = {}
+
+  var Broadcast = {
+    broadcast: {
+      document: function(docId) {
+        if(!docStreams[docId]) {
+          docStreams[docId] = through(function(buf, enc, cb) {
+            this.push(buf)
+            cb()
+          })
+        }
+        return docStreams[docId]
+      }
+    , sync: function(docId) {
+        if(!syncStreams[docId]) {
+          syncStreams[docId] = through(function(buf, enc, cb) {
+            this.push(buf)
+            cb()
+          })
+        }
+        return syncStreams[docId]
+      }
+    }
+  }
+  register(null, Broadcast)
 }
