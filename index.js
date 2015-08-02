bbec069c6e42c6a629d2677732e35379113a72f7
Marcel Klehr <mklehr@gmx.net>
2015-08-02 14:37:24 +0200

    Revamp to interface v3: Don't send messages to their origin
    it's now required to create one broadcast stream per client

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 var through = require('through2')
+  , duplexify = require('duplexify')
+  , PassThrough = require('stream').PassThrough
 
 module.exports = setup
 module.exports.provides = ['broadcast']
 
 function setup(plugin, imports, register) {
-  var docStreams = {}
-    , syncStreams = {}
+  var syncStreams = {}
+  var broadcasts = {}
 
   var Broadcast = {
     broadcast: {
-      document: function(docId) {
-        if(!docStreams[docId]) {
-          docStreams[docId] = through(function(buf, enc, cb) {
-            this.push(buf)
-            cb()
-          })
-        }
-        return docStreams[docId]
+      broadcast: function(docId) {
+        var readBroadcast = new PassThrough
+        if(!Array.isArray(broadcasts[docId])) broadcasts[docId] = []
+        broadcasts[docId].push(readBroadcast)
+        var stream = duplexify(through(function(buf, enc, cb) {
+          if(Array.isArray(broadcasts[docId])) {
+            broacasts[docId].forEach(function(s) {
+              if(s === readBroadcast) return
+              s.write(buf)
+            })
+          }
+          cb()
+        }), readBroadcast)
+        stream.on('end', function() {
+          broadcasts[docId].splice(broadcasts[docId].indexOf(readBroadcast))
+        })
+        return stream
       }
     , sync: function(docId) {
         return through(function(buf, enc, cb) {
