4bc14694c07bafbcd926fcf991523fd767a02f75
Marcel Klehr <mklehr@gmx.net>
2015-08-06 13:30:02 +0200

    Outsource channel handling

--- index.js
+++ index.js
@@ -16,53 +16,15 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 var through = require('through2')
-  , MuxDmx = require('mux-dmx')
+
 
 module.exports = setup
-module.exports.provides = ['broadcast']
+module.exports.consumes = ['broadcast']
 
 function setup(plugin, imports, register) {
-  var syncStreams = {}
-  var broadcasts = {}
-    , channels = {}
-
-  var Broadcast = {
-    broadcast: {
-      registerChannel: function(id, fn) {
-        if(channels[id.toString('base64')]) return
-        channels[id.toString('base64')] = fn
-      }
-    , document: function(docId, user) {
-        var b = MuxDmx()
-        if(!Array.isArray(broadcasts[docId])) broadcasts[docId] = []
-        broadcasts[docId].push(b)
-
-        Object.keys(channels).forEach(function(channel) {
-          var id = new Buffer(channel, 'base64')
-            , readable = b.createDuplexStream(id)
-            , writable = through(function(buf, enc, cb) {
-                if(Array.isArray(broadcasts[docId])) {
-                  broadcasts[docId].forEach(function(s) {
-                    if(s === b) return
-                    s.createDuplexStream(id).write(buf)
-                  })
-                }
-                cb()
-              })
-          channels[channel](user, docId, readable, writable)
-        })
-
-        b.on('end', function() {
-          broadcasts[docId].splice(broadcasts[docId].indexOf(b), 1)
-        })
-        return b
-      }
-    , sync: function(docId) {
-        return through(function(buf, enc, cb) {
-          cb()
-        })
-      }
-    }
-  }
-  register(null, Broadcast)
+  var broadcast = imports.broadcast
+  broadcast.registerTransport(through(function(buf, enc, cb){
+    cb()
+  }))
+  register()
 }
